3082d567f9adbd7ae56491f98d305178678c2c8d
BernardoVindex <ber.cardenas94@gmail.com>
2021-12-21 12:53:19 -0600

    Finish

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+const path = require('path')
 const HtmlWebpackPluin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
 
 module.exports = {
   output: {
@@ -33,6 +35,19 @@ module.exports = {
   plugins: [
     new HtmlWebpackPluin({
       template: 'src/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Tu app de fotos de mascotas',
+      shortname: 'Petgram UwU',
+      description: 'Con Petgram puedes encontrar fotos de tus animales domesticos muy fácilmente',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
     })
   ],
   devServer: {
